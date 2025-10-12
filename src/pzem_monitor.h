47c7f2e7de4888d60abe91411ab85ed769195ef3
root <a.karnauh@gmail.com>
2025-10-12 17:06:03 +0300

    Initial commit

---
+++ src/pzem_monitor.h
@@ -0,0 +1,118 @@
+#ifndef PZEM_MONITOR_H
+#define PZEM_MONITOR_H
+
+//#define _POSIX_C_SOURCE 200112L // Для strdup и usleep
+
+#include <stdio.h>
+#include <errno.h>
+#include <modbus/modbus.h>
+#include <unistd.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <syslog.h>
+
+// Размер буфера логов (15-20 строк)
+#define LOG_BUFFER_SIZE 20
+
+// Структура для хранения конфигурации
+typedef struct {
+    char tty_port[64];
+    int baudrate;
+    int slave_addr;
+    int poll_interval_ms;
+    char log_dir[256];
+    
+    // Чувствительность изменений
+    float voltage_sensitivity;
+    float current_sensitivity;
+    float frequency_sensitivity;
+    float power_sensitivity;
+    
+    // Пороги напряжения
+    float voltage_high_alarm;
+    float voltage_high_warning;
+    float voltage_low_warning;
+    float voltage_low_alarm;
+    
+    // Пороги тока
+    float current_high_alarm;
+    float current_high_warning;
+    float current_low_warning;
+    float current_low_alarm;
+    
+    // Пороги частоты
+    float frequency_high_alarm;
+    float frequency_high_warning;
+    float frequency_low_warning;
+    float frequency_low_alarm;
+} pzem_config_t;
+
+// Структура для хранения данных
+typedef struct {
+    float voltage;
+    float current;
+    float frequency;
+    float power;
+    int status;
+    int first_read;
+    
+    // Состояния порогов
+    char voltage_state;  // 'H' - высокое, 'L' - низкое, 'N' - норма
+    char current_state;  // 'H' - высокое, 'L' - низкое, 'N' - норма  
+    char frequency_state; // 'H' - высокое, 'L' - низкое, 'N' - норма
+} pzem_data_t;
+
+// Структура для буферизации логов
+typedef struct {
+    char **buffer;
+    int size;
+    int capacity;
+    int write_index;
+    char log_dir[256];
+    char config_name[64];
+} log_buffer_t;
+
+// Глобальные переменные
+extern modbus_t *ctx;
+extern volatile int keep_running;
+extern log_buffer_t log_buffer;
+extern pzem_config_t global_config;
+extern char *service_name;
+extern char config_name[64];
+
+// Функции конфигурации
+int load_config(const char *config_file, pzem_config_t *config);
+int create_directory_if_not_exists(const char *path);
+int validate_config(const pzem_config_t *config);
+void extract_config_name(const char *config_path);
+
+// Функции работы с логами
+int init_log_buffer(log_buffer_t *buffer, int initial_capacity, const char *log_dir);
+int add_to_log_buffer(log_buffer_t *buffer, const char *log_entry);
+int flush_log_buffer(log_buffer_t *buffer);
+void free_log_buffer(log_buffer_t *buffer);
+void get_current_date(char *date_str, size_t size);
+void get_current_time(char *time_str, size_t size);
+void get_log_file_path(char *path, size_t size, const char *log_dir);
+void prepare_log_entry(char *log_entry, size_t size, const pzem_data_t *data);
+int should_flush_buffer(const log_buffer_t *buffer);
+
+// Функции Modbus
+int init_modbus_connection(const pzem_config_t *config);
+int read_pzem_data(pzem_data_t *data);
+void cleanup(void);
+void safe_reconnect(const pzem_config_t *config);
+
+// Функции обработки данных
+int values_changed(const pzem_data_t *current, const pzem_data_t *previous, const pzem_config_t *config);
+void update_threshold_states(pzem_data_t *data, const pzem_config_t *config);
+int threshold_states_changed(const pzem_data_t *current, const pzem_data_t *previous);
+
+// Сигналы
+void signal_handler(int sig);
+
+#endif
